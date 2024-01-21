cdd9eeb264aab99225b7034c486603ca245e5c8c
Ben Lipman <benlipman35@gmail.com>
2024-01-21 11:23:43 -0700

    basic setup

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from "react"; 
+import Intro from "./components/intro";
+import Experience from "./components/Experience";
+import Portfolio from "./components/portfolio";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+
+function App() {
+
+  return (
+    <div className="App">
+      <Intro />
+      <Experience /> 
+    </div>
+  )
+}
+
+export default App;
